03e1de9ea5ec5ac5fb7a7c4fd847a9f9b8adc450
Loris Beltramo <loris.beltramo@dncsrl.com>
2026-04-02 12:27:26 +0200

    add log object

--- src/index.js
+++ src/index.js
@@ -13,25 +13,39 @@ app.use((req, res, next) => {
   console.log('Body:', req.body);
   next();
 });
-
+const generateLogObject = (req) => {
+  return {
+    method: req.method,
+    url: req.url,
+    headers: req.headers,
+    query: req.query,
+    params: req.params,
+    body: req.body
+  };
+}
 // default route for testing
 app.get('/', (req, res) => {
+    const logObject = generateLogObject(req);
   res.send('Hello World!');
 });
 app.get('/api/data', (req, res) => {
-  res.json({ message: 'GET request received' });
+  const logObject = generateLogObject(req);
+  res.json({ message: 'GET request received', log: logObject });
 });
 
 app.post('/api/data', (req, res) => {
-  res.json({ message: 'POST request received' });
+  const logObject = generateLogObject(req);
+  res.json({ message: 'POST request received', log: logObject });
 });
 
 app.put('/api/data/:id', (req, res) => {
-  res.json({ message: 'PUT request received' });
+  const logObject = generateLogObject(req);
+  res.json({ message: 'PUT request received', log: logObject });
 });
 
 app.delete('/api/data/:id', (req, res) => {
-  res.json({ message: 'DELETE request received' });
+  const logObject = generateLogObject(req);
+  res.json({ message: 'DELETE request received', log: logObject });
 });
  
 
